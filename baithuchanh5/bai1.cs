d0a87a44fb44eed34b0b7014844bc3a307835dc7
MINH QUAN <daingufgh3@gmail.com>
2021-10-18 15:35:47 +0700

    hello

---
+++ baithuchanh5/bai1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baithuchanh5
+{
+    class bai1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nhap so nguyen n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool kq = ktsonguyen(n);
+            Console.WriteLine(kq);
+        }
+        static bool ktsonguyen(int n)
+        {
+            if(n<2)
+            {
+                return false;
+            }
+            if (n % n == 0 || n % 1 == 0)
+            {
+                return true;
+            }
+            else
+                return false;             
+        }
+    }
+}
